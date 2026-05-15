304717b588268c8f81f6d06882e3bf10115e1832
Keneil2 <Keneilsamms23@gmail.com>
2026-05-14 22:23:33 -0500

    Remove unused dotenv import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,4 @@
 import axios from "axios"
-import process from "dotenv"
-const env =process.configDotenv();
 // create axios instance
 export const axiosInstance = axios.create({
  baseURL: import.meta.env.VITE_API_URL,
